729f99c8e97c982f89902206e6a195e94c152d74
AddAcTech <aguilar.chavez.alexis.daniel@gmail.com>
2024-01-15 00:26:45 -0600

    imagenes en el pdf, todo?:proteger ruta admin

--- src/components/InvitationPage.jsx
+++ src/components/InvitationPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Page, Text, View, StyleSheet } from "@react-pdf/renderer";
+import { Page, Text, View, StyleSheet, Image } from "@react-pdf/renderer";
 
 const styles = StyleSheet.create({
   page: {
@@ -8,6 +8,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 30,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "center",
+    fontFamily: "Helvetica-Bold", // Aplica negrita al título
+    marginBottom: 15,
   },
   text: {
     margin: 12,
@@ -28,19 +34,37 @@ function InvitationPage({
   return (
     <Page style={styles.page}>
       <View>
-        <Text>GALA DEL INSTITUTO POLITECNICO NACIONAL</Text>
-        <Text style={styles.text}>
-          Invitación para el invitado de {name} con ID: {id} número:
-          {guestNumber}
+        <Image
+          style={styles.image}
+          src="src\assets\gala.png" // Reemplaza esto con la ruta a tu imagen
+        />
+        <Text style={styles.title}>
+          GALA DEL INSTITUTO POLITECNICO NACIONAL
         </Text>
-        <Text>
-          Ubicado en {event_location} se le otorgará la presea {award}
+        <Text style={styles.text}>
+          Nos complace invitar al distinguido {name} con ID: {id} a nuestra gala
+          anual.
+        </Text>
+        <Text style={styles.text}>
+          Este evento se llevará a cabo en {event_location}. Durante la gala, se
+          le otorgará la presea {award}.
         </Text>
         {apoyo === "Necesito apoyo" ? (
-          <Text>Se le brindará apoyo por su Condicion fisica</Text>
+          <Text style={styles.text}>
+            Se le brindará apoyo debido a su condición física.
+          </Text>
         ) : (
-          <Text>No necesita apoyo</Text>
+          <Text style={styles.text}>
+            Hemos notado que no necesita apoyo adicional.
+          </Text>
         )}
+        <Text style={styles.text}>
+          Esperamos contar con su presencia para celebrar este evento especial.
+        </Text>
+        <Image
+          style={styles.image}
+          src="src\assets\ipn.jpg" // Reemplaza esto con la ruta a tu imagen
+        />
       </View>
     </Page>
   );
